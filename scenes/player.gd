531f9729fb9b97dbb28e558cb60586177bb83739
Ehab Aijaz <clutch.10306@gmail.com>
2026-01-21 14:12:44 +0530

    Changed player movement with velocity and added map borders

--- scenes/player.gd
+++ scenes/player.gd
@@ -1,9 +1,10 @@
-extends Node2D
+extends CharacterBody2D
 
-var direction : Vector2 = Vector2(1,0)
-var speed : int = 5
+var direction : Vector2 = Vector2(1,1)
+var speed : int = 200
 
 func _physics_process(delta):
 	direction = Input.get_vector("left", "right", "up", "down")
-	position += direction * speed
+	velocity = direction * speed
+	move_and_slide()
 	
